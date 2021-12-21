cf9b6f8581e7bf1db1f62b023d872cdc967e30e7
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2021-12-21 17:21:44 +0100

    FEAT :: added the bare-bones og greetings-vue

---
+++ src/components/GreetingsVue.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+       age is {{age}}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      age: "",
+      date : new Date()
+    };
+  },
+  props: {
+    username: {
+      type: String,
+      required: false,
+    },
+  },
+  created(){
+      this.age = 76
+  }
+};
+</script>
+
+<style scoped>
+</style>
