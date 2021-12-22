006251652ff09886161be4da5ee5c01cf3e504d3
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2021-12-22 10:53:00 +0100

    UPDATE :: renamed coponent to greetings-vue

--- src/entry.esm.js
+++ src/entry.esm.js
@@ -1,6 +1,6 @@
 
 // Import vue component
-import component from '@/vue-greetings.vue';
+import component from '@/VueGreetings.vue';
 
 // Default export is installable instance of component.
 // IIFE injects install function into component, allowing component
